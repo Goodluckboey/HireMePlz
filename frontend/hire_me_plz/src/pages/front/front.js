08b2c532256ae7716734671b6af7c630b125b1b4
PatrickB <84555143+Goodluckboey@users.noreply.github.com>
2022-01-13 09:43:13 +0800

    Test

--- frontend/hire_me_plz/src/pages/front/front.js
+++ frontend/hire_me_plz/src/pages/front/front.js
@@ -1,3 +1,43 @@
 import React from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+
+const FrontPage = () => {
+  return (
+    <div id="FrontPage">
+      <Splide
+        options={{
+          // gap: "0rem",
+          speed: "3000",
+          autoplay: true,
+          type: "loop",
+          pauseOnHover: true,
+          wheel: true,
+          // interval: 10,
+          drag: true,
+        }}
+      >
+        <SplideSlide
+          data-aos="fade-right"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+        ></SplideSlide>
+        <SplideSlide
+          data-aos="fade-right"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+        ></SplideSlide>
+        <SplideSlide
+          data-aos="fade-right"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+        ></SplideSlide>
+      </Splide>
+    </div>
+  );
+};
+
+export default FrontPage;
