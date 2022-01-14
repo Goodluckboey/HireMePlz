7250940fb3cedf282a0fde3624c2df1717811bf1
ruibing <rbruibingnet@gmail.com>
2022-01-14 22:41:50 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,24 +14,60 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+// get all jobs
+app.get("/alljobs", async (req, res) => {
+  try {
+    const jobs = await Job.find({});
+    res.json(jobs);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// search for a job
+app.post("/searchjobs", async (req, res) => {
+  const {}
+  try {
+    const jobs = await
+  }
+})
+
 // login
 app.post("/login", async (req, res) => {
   const { username, hash } = req.body;
-  console.log(username, hash);
-  const user = await User.findOne({ username });
-  console.log(user);
-  const valid = await bcrypt.compare(hash, user.hash);
+  let valid = false;
+  try {
+    const user = await User.findOne({ username });
+    valid = await bcrypt.compare(hash, user.hash);
+  } catch (err) {
+    console.log(err);
+  }
   if (valid) {
     res.json({ valid });
-    return;
+  } else {
+    res.json({ valid });
   }
-  res.json({ valid });
 });
 
 // myjobs
 app.get("/myjobs/:userid", async (req, res) => {
-  const jobs = await Job.find({ employerid: req.params.userid });
-  res.json(jobs);
+  try {
+    const jobs = await Job.find({ employerid: req.params.userid });
+    res.json(jobs);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// postjobs
+app.post("/postjobs", async (req, res) => {
+  try {
+    const newJob = new Job(req.body);
+    await newJob.save();
+    res.json(newJob);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // individualjobs
@@ -51,6 +87,7 @@ app.post("/registration", async (req, res) => {
   await User.create({ ...req.body, hash: hashed });
   res.send(req.body);
 });
+
 
 const PORT = 5000;
 app.listen(PORT, () => {
