f4494ce4c0535f7c799fad469b3a4aec08320e61
ruibing <rbruibingnet@gmail.com>
2022-01-14 22:45:45 +0800

    converted seeddata to use axios fetch instead

--- frontend/hire_me_plz/src/pages/employee-Market/EmployeeMarket.js
+++ frontend/hire_me_plz/src/pages/employee-Market/EmployeeMarket.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
-import seedData from "../../Seed/MyJobsSeed";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Job from "../myjobs/parts/Job";
 
 const EmployeeMarket = () => {
   const [jobQuery, setJobQuery] = useState("");
+  const [fetchedJobs, setFetchedJobs] = useState([]);
+
+  useEffect(() => {
+    async function fetcher() {
+      try {
+        const endpoint = `http://127.0.0.1:5000/alljobs`;
+        const res = await axios.get(endpoint);
+        setFetchedJobs(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetcher();
+  }, []);
+
+  const handleSearchJob =  async () => {
+    const endpoint = `http://127.0.0.1:5000/`
+    const jobs =  await axios.get()
+  }
+
   const jobs = [];
-  for (const job of seedData) {
+  for (const job of fetchedJobs) {
     jobs.push(<Job {...job}></Job>);
   }
   return (
