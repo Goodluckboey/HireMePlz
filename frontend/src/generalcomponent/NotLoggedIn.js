ac00703d5fa3944a0db2bce439fbf6293808f71e
PatrickB <84555143+Goodluckboey@users.noreply.github.com>
2022-01-16 16:17:08 +0800

    new folder structure

---
+++ frontend/src/generalcomponent/NotLoggedIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotLoggedIn = () => {
+  return (
+    <div>
+      <h1>
+        You are not logged in! Please login <Link to="/login">here</Link>
+      </h1>
+    </div>
+  );
+};
+
+export default NotLoggedIn;
