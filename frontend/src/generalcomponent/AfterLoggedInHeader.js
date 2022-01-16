19d6fb7cc8f1f3441405ad2f2ec6f59a1147f438
ignatiusyap <31462590+ignatiusyap@users.noreply.github.com>
2022-01-16 17:52:04 +0800

    Updated with logout function
    Unique key for username. Logout function works

--- frontend/src/generalcomponent/AfterLoggedInHeader.js
+++ frontend/src/generalcomponent/AfterLoggedInHeader.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import Button from "./Button";
+import Useridcontext from "../context/userid-context";
 
 const AfterLoggedInHeader = () => {
+  //Call the setUserId state from app
+  const callSetUserId = useContext(Useridcontext);
+  const setUserId = callSetUserId.setUserId;
+  //Reset state to blank upon logout
+  const handleLogout = () => {
+    setUserId("");
+  };
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
       <Link to="/">
@@ -9,7 +18,9 @@ const AfterLoggedInHeader = () => {
       </Link>
       <Link to="/profile">Profile</Link>
       <Link to="/myjobs">My Jobs</Link>
-      <Link to="/">Logout</Link>
+      <Link to="/">
+        <Button onClick={handleLogout} value="Logout"></Button>
+      </Link>
     </div>
   );
 };
