1a45a20b9c1985c6c8d7966ff0bf849e6f3499da
PatrickB <84555143+Goodluckboey@users.noreply.github.com>
2022-01-16 21:57:31 +0800

    frontpage draft design

--- frontend/src/pages/front/front.js
+++ frontend/src/pages/front/front.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import FrontPageHeader from "./parts/FrontPageHeader";
+import styles from "./parts/modules/header.module.css";
 import axios from "axios";
 import Job from "../employeemarketplace/parts/Job";
 import { v4 as uuidv4 } from "uuid";
@@ -7,72 +8,22 @@ import Useridcontext from "../../context/userid-context";
 
 const FrontPage = () => {
   const callAndSetUserId = useContext(Useridcontext);
-  const userId = callAndSetUserId.userId;
-  let [foundJobData, setFoundJobData] = useState("");
-  const [jobQuery, setJobQuery] = useState("");
-
-  // sends searchquery data into state onChange
-  // calls the searchAPI with the searchquery data
-  // runs map on searchResults, and produces showdata on mount(entire function should be a useEffect)
-
-  // const pullJobData = () => {
-  //   async function fetcher() {
-  //     try {
-  //       const jobs = await axios.post(`http://localhost:5000/searchjobs`, {
-  //         query: jobQuery,
-  //       });
-  //       setFoundJobData(jobs);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   }
-  //   fetcher();
-  // };
-
-  // const attachEmployeeId = (jobId) => {
-  //   const userIdToAttach = {
-  //     userId: userId,
-  //   };
-  //   axios
-  //     .put(`http://127.0.0.1:5000/applyjob/${jobId}`, userIdToAttach)
-  //     .then(() => {
-  //       async function fetcher() {
-  //         try {
-  //           const endpoint = `http://127.0.0.1:5000/alljobs`;
-  //           const res = await axios.get(endpoint);
-  //           setFoundJobData(res.data);
-  //         } catch (err) {
-  //           console.log(err);
-  //         }
-  //       }
-  //       fetcher();
-  //     });
-  // };
-
-  // const jobs = [];
-  // if (foundJobData === true) {
-  //   for (const element of foundJobData) {
-  //     jobs.push(
-  //       <Job
-  //         {...element}
-  //         key={uuidv4()}
-  //         onClick={() => attachEmployeeId(element._id)}
-  //       ></Job>
-  //     );
-  //   }
-  // }
 
   return (
     <div id="FrontPage">
-      <div>
-        <FrontPageHeader></FrontPageHeader>
-      </div>
-      <div>
+      <FrontPageHeader></FrontPageHeader>
+      <div className={styles.banner}>
+        <img id={styles.bannerImage} src={"/images/pngItem_2949029.jpg"}></img>
+        <h1 id={styles.titleText}>
+          Be part of the Job search <br />
+          with a helpful community!
+        </h1>
         <form>
           <input
-            id="searchbar"
+            id={styles.searchbar}
             type="text"
-            placeholder="Search.."
+            placeholder={`Try entering "fixer"..`}
+            size="80"
             // onChange={(e) => {
             //   setJobQuery(e.target.value);
             // }}
@@ -80,8 +31,22 @@ const FrontPage = () => {
           ></input>
         </form>
       </div>
-      <div>
+      <div id={styles.whyChooseUs}>
         <h1>Why Choose Us?</h1>
+        <div id={styles.iconsBox}>
+          <div className="iconContainer">
+            <i class="fas fa-arrows-alt fa-7x"></i>
+            <p>Text for Icon</p>
+          </div>
+          <div className="iconContainer">
+            <i class="fas fa-bed fa-7x"></i>
+            <p>Text for Icon</p>
+          </div>
+          <div className="iconContainer">
+            <i class="fas fa-bicycle fa-7x"></i>
+            <p>Text for Icon</p>
+          </div>
+        </div>
       </div>
     </div>
   );
