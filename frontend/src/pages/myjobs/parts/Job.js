bd3e698b2e9b06b6456e897235b20e36a4cf6179
PatrickB <84555143+Goodluckboey@users.noreply.github.com>
2022-01-18 21:06:33 +0800

    myJobs just started

--- frontend/src/pages/myjobs/parts/Job.js
+++ frontend/src/pages/myjobs/parts/Job.js
@@ -6,31 +6,31 @@ const Job = ({ name, description, reward, status, applyJob, imageUrl }) => {
   const imgStyle = { width: "100px", height: "100px", objectFit: "cover" };
 
   return (
-    <div className={styles.card}>
+    <div>
       <div className={styles.cardImgBox}>
         <img src={imageUrl} className={styles.cardImg} alt="myImage" />
       </div>
       <div className={styles.cardInfo}>
-        <h3 className={styles.cardName}>{name}</h3>
-        <h3 className={styles.cardReward}>For {reward} coins</h3>
-        <h3 className={styles.cardDescription}>{description}</h3>
-        {status === "Open" && (
-          <h5>
-            {" "}
-            <span class="badge rounded-pill bg-light text-dark">{status}</span>
-          </h5>
-        )}
-        {status === "accepted" && (
-          <h5>
-            <span class="badge rounded-pill bg-success text-dark ">
-              {status}
-            </span>
-          </h5>
-        )}
+        <div className={styles.ctrlBadge}>
+          {status === "Open" && (
+            <h5>
+              <span class="badge rounded-pill bg-light text-dark">
+                {status}
+              </span>
+            </h5>
+          )}
+          {status === "accepted" && (
+            <h5>
+              <span class="badge rounded-pill bg-success text-dark ">
+                {status}
+              </span>
+            </h5>
+          )}
+          <p className={styles.cardName}>{name}</p>
+        </div>
+        <p className={styles.cardDescription}>{description}</p>
+        <h4 className={styles.cardReward}>GC {reward} </h4>
       </div>
-      {/* <button onClick={applyJob} type="button" class="btn btn-success">
-        Apply
-      </button> */}
     </div>
   );
 };
