1168fa1b6eb0f9c519e884ecb835a31fb45d705f
PatrickB <84555143+Goodluckboey@users.noreply.github.com>
2022-01-18 14:56:56 +0800

    IndividualJobs, appliedIndiJobs draft

--- frontend/src/pages/individualapplied/IndividualAppliedJob.js
+++ frontend/src/pages/individualapplied/IndividualAppliedJob.js
@@ -9,14 +9,20 @@ import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router";
 import NotLoggedIn from "../../generalcomponent/NotLoggedIn";
+import AfterLoggedInHeader from "../../generalcomponent/AfterLoggedInHeader.js";
 
 const AppliedIndividualJob = (props) => {
   //UseContext
-  const { userId, picsArray } = useContext(Useridcontext);
+  const { userId, picsArray, switchMode, setSwitchMode } =
+    useContext(Useridcontext);
 
   // save state
   const [appliedJobsData, setAppliedJobsData] = useState("");
   const params = useParams();
+
+  const handleModeChange = () => {
+    setSwitchMode(false);
+  };
 
   const getJobsDataUnderUser = () => {
     async function fetcher() {
@@ -48,25 +54,63 @@ const AppliedIndividualJob = (props) => {
     <div>
       {userId ? (
         <>
-          <Link to="/myjobs">My Jobs</Link>
+          {/* <Link to="/myjobs">My Jobs</Link> */}
+          <AfterLoggedInHeader></AfterLoggedInHeader>
+          <div id={styles.sidebar}>
+            <h1 className={styles.title}>My Applied Jobs</h1>
+            <p>You are viewing as an Employee</p>
+            <p>Click on each job for more information</p>
+            <div className={styles.buttonGroup}></div>
+            <Link to="/myjobs">
+              <button
+                type="button"
+                class="btn btn-outline-dark col-11 mx-auto"
+                data-mdb-ripple-color="dark"
+              >
+                Back
+              </button>
+            </Link>
+          </div>
           {appliedJobsData && (
             <Splide
               options={{
                 start: params.index,
                 wheel: true,
+                perPage: 3,
+                perMove: 2,
+                gap: "10px",
+                slideFocus: true,
               }}
+              className={styles.mainSplider}
             >
               {appliedJobsData.map((element) => {
                 return (
                   <>
                     <SplideSlide>
                       <div className={styles.largeJob}>
-                        <h1>{element.name}</h1>
-                        <p className={styles.description}>
-                          Job Description: {element.description}
+                        <p class="note note-light">
+                          <h3 className={styles.Title}>{element.name}</h3>
                         </p>
-                        <h2>{element.reward} Copper Coins</h2>
-                        <h2>{element.status}</h2>
+                        <p class="note note-info">
+                          <p className={styles.description}>
+                            Job Description: {element.description}
+                          </p>
+                          <p>{element.reward} Copper Coins</p>
+                          {element.status === "Open" && (
+                            <p>
+                              <span class="badge rounded-pill bg-light text-dark">
+                                {element.status}
+                              </span>
+                            </p>
+                          )}
+                          {element.status === "accepted" && (
+                            <p>
+                              <span class="badge rounded-pill bg-success text-dark ">
+                                {element.status}
+                              </span>
+                            </p>
+                          )}
+                        </p>
                         <Button
                           onClick={() => handleUnapply(element._id)}
                           value="Cancel Application"
